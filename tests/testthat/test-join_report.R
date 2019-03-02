f0a9030e10892c96cde80e687424ffa45f21e6d5
Matthew Lowes <matt.lowes@oneacrefund.org>
2019-03-01 20:40:09 -0500

    adds test for join_report, wip

---
+++ tests/testthat/test-join_report.R
@@ -0,0 +1,14 @@
+context("join_report")
+
+# input -----------------------------------------
+
+# wip << not sure how to best test cat output
+
+# test_that("join_report returns the right number of rows for each df", {
+#   data("band_members")
+#
+#   test <- band_members %>%
+#     join_report(., left_join(., band_instruments, by = "name"))
+#
+# })
+
